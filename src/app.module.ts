7ecf1c8165c3b5b86888ba63bfbce4b18cefc82f
aixion1506 <aixion1506@gmail.com>
2025-02-03 14:49:30 +0900

    Feat: 환경 변수 유효성 검사 추가 및 ConfigModule 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,44 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { z } from 'zod';
+import { DatabaseModule } from './db/db.module';
+
+const configSchema = z.object({
+  BACKEND_PORT: z.number().min(1).max(65535),
+  POSTGRES_HOST: z.string().nonempty(),
+  POSTGRES_PORT: z.number().min(1).max(65535),
+  POSTGRES_USER: z.string().nonempty(),
+  POSTGRES_PASSWORD: z.string().nonempty(),
+  POSTGRES_DB: z.string().nonempty(),
+});
+
+// 유효성 검사 함수를 생성합니다.
+const validateConfig = (config: Record<string, unknown>) => {
+  // 환경 변수를 숫자로 변환합니다.
+  const parsedConfig = {
+    ...config,
+    BACKEND_PORT: Number(config.BACKEND_PORT),
+    POSTGRES_PORT: Number(config.POSTGRES_PORT),
+  };
+
+  const result = configSchema.safeParse(parsedConfig);
+  if (!result.success) {
+    throw new Error(
+      `Configuration validation error: ${JSON.stringify(result.error.errors)}`,
+    );
+  }
+  return result.data;
+};
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: validateConfig, // 여기서 유효성 검사 함수를 사용합니다.
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
