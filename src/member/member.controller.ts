1771b0b51881e407d9d20d3a7ca3ee4a90a6c513
aixion1506 <aixion1506@gmail.com>
2025-02-03 18:24:42 +0900

    BE-7 Feat: 회원가입 Controller/Service 구현

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { MemberService } from './member.service';
+import { Member } from './entity/member.entitiy';
 
 @Controller('member')
-export class MemberController {}
+export class MemberController {
+  constructor(private readonly memberService: MemberService) {}
+
+  @Post()
+  async create(@Body() memberData: Partial<Member>) {
+    return this.memberService.create(memberData);
+  }
+}
--- src/member/member.service.ts
+++ src/member/member.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Member } from './entity/member.entitiy';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  constructor(@InjectRepository(Member) private reop: Repository<Member>) {}
+
+  async create(memberData: Partial<Member>): Promise<Member> {
+    const member = this.reop.create(memberData);
+    return await this.reop.save(member);
+  }
+}
