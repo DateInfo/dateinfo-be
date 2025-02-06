0a3f51c9d46a0697c3cb16f4c7f11bb38936c7ab
aixion1506 <aixion1506@gmail.com>
2025-02-06 15:51:22 +0900

    Feat: Swagger 셋팅

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,13 +1,21 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { MemberService } from './member.service';
-import { Member } from './entity/member.entitiy';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateMemberDto } from './dtos/create-member.dto';
 
+@ApiTags('members')
 @Controller('member')
 export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 
   @Post()
-  async create(@Body() memberData: Partial<Member>) {
-    return this.memberService.create(memberData);
+  @ApiOperation({ summary: 'Create a new member' }) // API 설명 추가
+  @ApiResponse({
+    status: 201,
+    description: 'The member has been successfully created.',
+  })
+  @ApiResponse({ status: 400, description: 'Bad Request.' })
+  async create(@Body() body: CreateMemberDto) {
+    return this.memberService.create(body);
   }
 }
