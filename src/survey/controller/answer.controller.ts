449cf8ff0508ea516a9fc21c68f1933786eecf43
aixion1506 <aixion1506@gmail.com>
2025-03-04 23:04:53 +0900

    Docs: 설문 응답 저장 API Swagger 문서 및 DTO 구조 개선

--- src/survey/controller/answer.controller.ts
+++ src/survey/controller/answer.controller.ts
@@ -1,7 +1,13 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse, ApiParam } from '@nestjs/swagger';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiParam,
+  ApiBody,
+} from '@nestjs/swagger';
 import { AnswerService } from '../services/answer.service';
-import { CreateAnswerDto } from '../dtos/create-answer.dto';
+import { CreateSurveyAnswerDto } from '../dtos/create-answer.dto';
 import { Answer } from '../entities/answer.entity';
 
 @ApiTags('Answer')
@@ -9,15 +15,73 @@ import { Answer } from '../entities/answer.entity';
 export class AnswerController {
   constructor(private readonly answerService: AnswerService) {}
 
-  @ApiOperation({ summary: '설문조사 답변 생성' })
+  /**
+   * 설문 답변 제출 API
+   * @param createSurveyAnswerDto - 설문 답변 DTO
+   */
+  @Post()
+  @ApiOperation({
+    summary: '설문 응답 저장',
+    description:
+      '한 번의 요청으로 연애 성향 및 스타일 설문에 대한 모든 답변을 저장합니다.',
+  })
+  @ApiBody({
+    description: '연애 성향 및 스타일 설문 응답 예시',
+    examples: {
+      example: {
+        summary: '설문 응답 예제',
+        value: {
+          memberId: 18,
+          surveyId: 5,
+          answers: [
+            { questionId: 32, selectedOptionId: 64 },
+            { questionId: 33, selectedOptionId: 68 },
+            { questionId: 34, selectedOptionId: 70 },
+            { questionId: 35, selectedOptionId: 74 },
+            { questionId: 36, selectedOptionId: 78 },
+            { questionId: 38, selectedOptionId: 81 },
+            { questionId: 39, selectedOptionId: 85 },
+            { questionId: 40, selectedOptionId: 89 },
+            { questionId: 41, selectedOptionId: 92 },
+            { questionId: 42, selectedOptionId: 95 },
+            { questionId: 43, selectedOptionId: 99 },
+            { questionId: 45, selectedOptionId: 103 },
+            { questionId: 48, selectedOptionId: 106 },
+            {
+              questionId: 31,
+              answerText: 'MBTI가 성격을 설명하는 데 도움이 됩니다.',
+            },
+            {
+              questionId: 47,
+              answerText:
+                'MBTI 차이로 갈등이 생길 수도 있지만, 서로 배울 점도 많아요.',
+            },
+            {
+              questionId: 46,
+              answerText:
+                '제 MBTI가 감성적이라 연애할 때 상대의 감정을 많이 배려해요.',
+            },
+            {
+              questionId: 37,
+              answerText: '서로의 강점을 살려 역할을 나누면 좋겠어요.',
+            },
+            { questionId: 30, answerText: 'INFJ' },
+            { questionId: 44, answerText: '안정감과 자기 성장' },
+            {
+              questionId: 49,
+              answerText: '상대방의 MBTI가 저와 상호 보완적이길 원해요.',
+            },
+          ],
+        },
+      },
+    },
+  })
   @ApiResponse({
     status: 201,
-    description: '설문조사 답변이 생성되었습니다.',
-    type: Answer,
+    description: '설문 답변이 성공적으로 저장됨',
   })
-  @Post()
-  createAnswer(@Body() createAnswerDto: CreateAnswerDto): Promise<Answer> {
-    return this.answerService.createAnswer(createAnswerDto);
+  async submitAnswers(@Body() createSurveyAnswerDto: CreateSurveyAnswerDto) {
+    return this.answerService.create(createSurveyAnswerDto);
   }
 
   @ApiOperation({ summary: '설문조사 답변 전체 조회' })
